762a7cf345f8ee438ee10da1f30694d02b8dddaf
angelperez_cristobal <canvia.093@izipay.pe>
2025-05-21 08:15:30 -0500

    chore: remove unused imports to clean up codebase

--- api-price/src/main/java/com/inditex/core/adapter/persistence/repository/PriceRepository.java
+++ api-price/src/main/java/com/inditex/core/adapter/persistence/repository/PriceRepository.java
@@ -7,22 +7,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 
 public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
 
     /**
      * Busca los precios aplicables según el producto, la marca y la fecha de aplicación.
      * Retorna una lista ordenada por prioridad descendente.
-     *
      * @param productId identificador del producto
-     * @param brandId identificador de la marca
+     * @param brandId   identificador de la marca
      * @param dateQuery fecha de aplicación
      * @return lista de precios aplicables ordenados por prioridad
      */
     @Query(value = Constants.H2Query.FIND_APPLICABLE_PRICE)
     PriceEntity findApplicablePrice(@Param("productId") Long productId,
-                                              @Param("brandId") Long brandId,
-                                              @Param("dateQuery") LocalDateTime dateQuery);
+                                    @Param("brandId") Long brandId,
+                                    @Param("dateQuery") LocalDateTime dateQuery);
 }
